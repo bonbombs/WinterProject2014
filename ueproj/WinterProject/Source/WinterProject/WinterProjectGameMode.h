dcf30e57b9dc78c865525ab0fd6a0e712c6f3b5d
Benny Peake <rayantares13@gmail.com>
2014-12-25 17:07:47 -0500

    Project repo setup
    Tested By: Building Project

---
+++ ueproj/WinterProject/Source/WinterProject/WinterProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "WinterProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WINTERPROJECT_API AWinterProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
